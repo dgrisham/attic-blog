9cf6bb430c3934629ddf573ba0c1318d5713e38b
dgrisham <dmgrisham@gmail.com>
2020-04-08 14:37:08 -0600

    Generate latest posts in index.html

--- gen_author_index_files.sh
+++ gen_author_index_files.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env zsh
 
+cd html
 tab='    '
+index='index.html'
+
+echo '<!DOCTYPE html>\n<html>\n<body>' >$index
+echo '<h1>The Attic</h1>' >>$index
+echo '<h2>Latest Posts</h2>' >>$index
+
 for dir in posts/*/; do
     author=$(basename $dir)
     # index=/dev/stdout
-    index="$dir/index.html"
+    author_index="$dir/index.html"
 
-    echo '<!DOCTYPE html>\n<html>' >$index
-    echo "<h1>$author's Posts</h1>" >>$index
+    echo '<!DOCTYPE html>\n<html>' >$author_index
+    echo "<h1>$author's Posts</h1>" >>$author_index
 
     echo "Processing posts for author $author"
 
     # blog posts files, sorted in reverse alphabetical order (name of post should be date %Y-%M-%D.md)
     posts=("${(@f)$(find $dir -name '*.html' ! -name index.html | sort -r)}")
-    [[ -z "$posts" ]] && echo "No posts for author $author" && rm -f $index && continue
+    [[ -z "$posts" ]] && echo "No posts for author $author" && rm -f $author_index && continue
 
+    i=0
     for post in $posts; do
         title=$(ag -o '(?<=<h1>).*?(?=</h1>)' $post)
-        echo "$tab<a href=\"$post\">$title</a>" >>$index
+        if ((i==0)); then # put entry into main page's latest posts
+            echo "$tab<a href=\"$post\">$title</a> by $author" >>$index
+        fi
+        echo "$tab<a href=\"$post\">$title</a>" >>$author_index
+        ((i++))
     done
 
-    echo "</html>" >>$index
+    echo "</html>" >>$author_index
 done
+
+echo '</body>\n</html>' >>$index
+
+cd ..
