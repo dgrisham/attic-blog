293e804c59f2b7ce3291bff1a0b93d5fde1e857a
dgrisham <dmgrisham@gmail.com>
2020-04-08 14:46:05 -0600

    Update post name

--- gen_html.zsh
+++ gen_html.zsh
@@ -20,7 +20,7 @@ for dir in posts/*/; do
 
     echo "Processing posts for author $author"
 
-    # blog posts files, sorted in reverse alphabetical order (name of post should be date %Y-%M-%D.md)
+    # blog posts files, sorted in reverse alphabetical order (name of post should start with date %Y-%M-%D*.html)
     posts=("${(@f)$(find $dir -name '*.html' ! -name index.html | sort -r)}")
     [[ -z "$posts" ]] && echo "No posts for author $author" && rm -f $author_index && continue
 
