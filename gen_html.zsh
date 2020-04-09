ddf766cfe685cd835b5cc6ac02e386e38e412fa9
dgrisham <dmgrisham@gmail.com>
2020-04-09 10:57:23 -0600

    Fix relative paths to posts

--- gen_html.zsh
+++ gen_html.zsh
@@ -13,21 +13,23 @@ echo '<h2>Latest Posts</h2>' >>$index
 echo '<body>' >>$index
 for dir in posts/*/; do
     dir=$(perl -pe 's|/$||' <(echo $dir))
+    echo "dir: $dir"
     author=$(basename $dir)
 
     echo "Processing posts for author $author"
 
     # blog posts files, sorted in reverse alphabetical order (name of post should start with date %Y-%M-%D*.html)
-    posts=("${(@f)$(find $dir -name '*.html' ! -name index.html | sort -r)}")
+    posts=("${(@f)$(find $dir -name '*.html' ! -name index.html -exec basename {} \; | sort -r)}")
     [[ -z "$posts" ]] && echo "No posts for author $author" && continue
 
     author_index="$dir/index.html"
+    echo "author_index: $author_index"
     echo '<!DOCTYPE html>\n<html>' >$author_index
     echo "<h1>$author's Posts</h1>" >>$author_index
 
     i=0
     for post in $posts; do
-        title=$(ag -o '(?<=<h1>).*?(?=</h1>)' $post | head -n1)
+        title=$(ag -o '(?<=<h1>).*?(?=</h1>)' $dir/$post | head -n1)
         if ((i==0)); then # put entry into main page's latest posts
             echo "$tab<a href=\"$post\">$title</a> by <a href=\"$author_index\">$author</a><br/>" >>$index
         fi
