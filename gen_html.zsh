14fa0fd8408e9ff5567f3b9db7c9fbf97d5ff62e
dgrisham <dmgrisham@gmail.com>
2020-04-09 15:02:22 -0600

    Use template files

--- gen_html.zsh
+++ gen_html.zsh
@@ -1,16 +1,14 @@
 #!/usr/bin/env zsh
 
+tab() {
+    printf '  %.0s' {1..$1}
+}
+
 cd html
-
-tab='    '
-
 index='index.html'
-echo '<!DOCTYPE html>\n<html>' >$index
-echo '<head><link rel="stylesheet" href="styles.css"></head>' >>$index
-echo '<header class="main-header" role="banner"><img src="img/logo.png"></header>' >>$index
-echo '<h2>Latest Posts</h2>' >>$index
+cp ../home.template.html $index
 
-echo '<body>' >>$index
+latest_posts=''
 for dir in posts/*/; do
     dir=$(perl -pe 's|/$||' <(echo $dir))
     author=$(basename $dir)
@@ -22,22 +20,21 @@ for dir in posts/*/; do
     [[ -z "$posts" ]] && echo "No posts for author $author" && continue
 
     author_index="$dir/index.html"
-    echo '<!DOCTYPE html>\n<html>' >$author_index
-    echo "<h1>$author's Posts</h1>" >>$author_index
-
+    cp ../author.template.html $author_index
     i=0
+    post_links=''
+    perl -pi -e "s|{author}|$author|" $author_index
     for post in $posts; do
         title=$(ag -o '(?<=<h1>).*?(?=</h1>)' $dir/$post | head -n1)
         if ((i==0)); then # put entry into main page's latest posts
-            echo "$tab<a href=\"$dir/$post\">$title</a> by <a href=\"$author_index\">$author</a><br/>" >>$index
+            latest_posts+="\n$(tab 2)<a href=\"$dir/$post\">$title</a> by <a href=\"$author_index\">$author</a><br/>"
         fi
-        echo "$tab<a href=\"$post\">$title</a><br/>" >>$author_index
+        post_links+="\n$(tab 2)<a href=\"$post\">$title</a><br/>"
         ((i++))
     done
-
-    echo "</html>" >>$author_index
+    perl -pi -e "s|{posts}|$post_links|" $author_index
 done
 
-echo '</body>\n</html>' >>$index
+perl -pi -e "s|{latest_posts}|$latest_posts|" $index
 
 cd ..
